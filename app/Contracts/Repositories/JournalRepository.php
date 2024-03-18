8caf5949e344373008e91fd78e535e76ad4aa4ec
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-03-18 09:28:03 +0700

    Update JournalRepository.php

--- app/Contracts/Repositories/JournalRepository.php
+++ app/Contracts/Repositories/JournalRepository.php
@@ -42,7 +42,7 @@ class JournalRepository extends BaseRepository implements JournalInterface
         }
 
         $data['user_id'] = auth()->user()->id;
-        $data['status'] = 'status';
+        $data['status'] = 'fillin';
         return $this->model->query()->create($data);
     }
     public function update(mixed $id, array $data): mixed
