5bf696b60dc9da2cafbce70cc26515dafe8f13f3
rendi <rendydwisaputra250@gmail.com>
2024-03-19 11:01:54 +0700

    register fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,9 +5,9 @@
         <div class="authentication-inner row">
             <div class="d-none d-lg-flex col-lg-7 p-0">
                 <div class="">
-                    <img src="{{ asset('assetsLogin/image.jpg') }}"
-                        alt="auth-login-cover" class="img-fluid my-5 auth-illustration auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center" style="object-fit: cover"
-                        data-app-dark-img="illustrations/auth-login-illustration-dark.html">
+                    <img src="{{ asset('assetsLogin/image.jpg') }}" alt="auth-login-cover"
+                        class="img-fluid my-5 auth-illustration auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center"
+                        style="object-fit: cover" data-app-dark-img="illustrations/auth-login-illustration-dark.html">
                 </div>
             </div>
             <div class="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
@@ -18,7 +18,8 @@
                         @csrf
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input type="text" class="form-control" id="email" name="email" placeholder="Masukkan email Anda" autofocus>
+                            <input type="text" class="form-control" id="email" name="email"
+                                placeholder="Masukkan email Anda" autofocus>
                             @error('username')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -29,7 +30,8 @@
                             <label for="email" class="form-label">Password</label>
                             <div class="input-group input-group-merge">
                                 <input type="password" id="password" class="form-control" name="password"
-                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
+                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                    aria-describedby="password" />
                                 <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
                                 @error('password')
                                     <div class="invalid-feedback">
@@ -41,83 +43,37 @@
                         <button type="submit" class="btn btn-primary d-grid w-100">
                             Masuk
                         </button>
+                        <a href="/register" class="text-black mt-2">Belum Punya akun? <span
+                                class="text-primary">Daftar</span></a>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+
+    @if (session('pending'))
+        <div id="myModal" class="modal fade show" tabindex="-1" aria-labelledby="myModalLabel" style="display: block;"
+            aria-modal="true" role="dialog">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content text-center">
+                    <div class="modal-header">
+                    </div>
+                    <div class="modal-body">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24"
+                            class="bg-success rounded-circle icon" fill="none" stroke="white" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round"
+                            class="icon icon-tabler icons-tabler-outline icon-tabler-check">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M5 12l5 5l10 -10" />
+                        </svg>
+                        <h4 class="text-dark fs-7 mb-0 mt-3">{{ session('pending') }}</h4>
+                    </div>
+                    <div class="modal-footer d-flex justify-content-center">
+                        <button type="button" class="btn btn-success" data-bs-dismiss="modal">OK</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    @endif
 @endsection
-
-{{-- @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection --}}
