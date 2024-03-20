fe1cca05c66a49d4398a39048f801da7c670902f
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-03-20 10:44:26 +0700

    user

--- database/migrations/2024_03_15_012313_create_journals_table.php
+++ database/migrations/2024_03_15_012313_create_journals_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\StatusJournalEnum;
+use App\enum\StatusJournalEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
