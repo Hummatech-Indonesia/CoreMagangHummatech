2a8216c4dea459db16d63f9c6b563ba43a6883a0
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-03-21 10:47:48 +0700

    login

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::get('users' , [UserController::class , 'index']);
+Route::post('login', [LoginController::class, 'login']);
