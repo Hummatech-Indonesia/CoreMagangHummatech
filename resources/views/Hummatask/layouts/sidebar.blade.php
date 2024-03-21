c606b7a5f0e6b318aba40d8b6ab4c1190a2a5a40
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-03-21 10:13:26 +0700

    api user

---
+++ resources/views/Hummatask/layouts/sidebar.blade.php
@@ -0,0 +1,37 @@
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div>
+      <!-- Sidebar navigation-->
+      <nav class="sidebar-nav scroll-sidebar container-fluid">
+        <ul id="sidebarnav">
+          <!-- ============================= -->
+          <!-- Home -->
+          <!-- ============================= -->
+          <li class="nav-small-cap">
+            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+            <span class="hide-menu">Home</span>
+          </li>
+          <!-- =================== -->
+          <!-- Dashboard -->
+          <!-- =================== -->
+          <li class="sidebar-item">
+            <a class="sidebar-link " href="/dashboard/task" aria-expanded="false">
+                <span>
+                    <i class="ti ti-aperture"></i>
+                </span>
+              <span class="hide-menu">Dashboard</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a class="sidebar-link " href="/team" aria-expanded="false">
+                <span>
+                    <i class="ti ti-aperture"></i>
+                </span>
+              <span class="hide-menu">Team</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- End Sidebar navigation -->
+    </div>
+</aside>
