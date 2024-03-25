f3e752488a470c23f16c3247cb761c58035691bd
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-03-25 13:32:13 +0700

    course

--- app/Contracts/Interfaces/CourseInterface.php
+++ app/Contracts/Interfaces/CourseInterface.php
@@ -4,11 +4,12 @@ namespace App\Contracts\Interfaces;
 use App\Contracts\Interfaces\Eloquent\CountInterface;
 use App\Contracts\Interfaces\Eloquent\DeleteInterface;
 use App\Contracts\Interfaces\Eloquent\GetInterface;
+use App\Contracts\Interfaces\Eloquent\GetWhereInterface;
 use App\Contracts\Interfaces\Eloquent\ShowInterface;
 use App\Contracts\Interfaces\Eloquent\StoreInterface;
 use App\Contracts\Interfaces\Eloquent\UpdateInterface;
 
-interface CourseInterface extends GetInterface , StoreInterface , DeleteInterface , UpdateInterface ,ShowInterface ,CountInterface
+interface CourseInterface extends GetInterface , StoreInterface , DeleteInterface , UpdateInterface ,ShowInterface ,CountInterface ,GetWhereInterface  
 {
 
 }
