a297afa26ce3b28828b8470837505420ef43eced
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-03-26 13:11:03 +0700

    slicing tantangan student offline

--- routes/farah.php
+++ routes/farah.php
@@ -80,6 +80,10 @@ Route::get('administrator/course/detail/sub-course', function() {
     return view('admin.page.course.sub-course.index');
 });
 
-Route::get('offlie-student/task', function(){
+Route::get('siswa-offline/task', function(){
     return view('student_offline.task.index');
+});
+
+Route::get('siswa-offline/challenge', function(){
+    return view('student_offline.challenge.index');
 });
