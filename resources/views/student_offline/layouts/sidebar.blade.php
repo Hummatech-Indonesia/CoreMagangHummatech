83f348db5297a1630ee6280ec5ee3bf50d4dfa50
nesaathussholeha <nesaathussholeha@gmail.com>
2024-03-27 04:57:42 +0700

    slicing siswa student offline

--- resources/views/student_offline/layouts/sidebar.blade.php
+++ resources/views/student_offline/layouts/sidebar.blade.php
@@ -125,7 +125,7 @@
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="#" class="sidebar-link">
+                            <a href="{{ url('siswa-offline/others/student')}}" class="sidebar-link">
                                 <div class="round-16 d-flex align-items-center justify-content-center">
                                     <i class="ti ti-circle"></i>
                                 </div>
