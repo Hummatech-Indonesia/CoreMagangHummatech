1cee51bce833e1a3641ead8f2f0ba152351c5098
nesaathussholeha <nesaathussholeha@gmail.com>
2024-04-02 09:50:44 +0700

    Update zoomschedule.blade.php

--- resources/views/mentor/zoomschedule.blade.php
+++ resources/views/mentor/zoomschedule.blade.php
@@ -1,31 +1,66 @@
 @extends('mentor.layouts.app')
 @section('content')
+
+<div class="row mb-3">
+    <div class="col-md-4 col-lg-2 col-xl-2">
+        <select class="form-select">
+            <option selected>Semua</option>
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+            <option value="3">Option 3</option>
+        </select>
+    </div>
+</div>
+
     <div class="row">
         <div class="col-4">
             <div class="card">
-                <div class="card-header text-bg-info d-flex align-items-center">
-                  <h4 class="card-title text-white mb-0">Belajar LARAVEL11</h4>
-                  <div class="card-actions cursor-pointer ms-auto d-flex button-group">
-                    <a href="javascript:void(0)" class="link text-white d-flex align-items-center" data-action="collapse"><i class="ti ti-minus fs-6"></i></a>
-                    <a href="javascript:void(0)" class="mb-0 btn-minimize px-2 cursor-pointer text-white link d-flex align-items-center" data-action="expand"><i class="ti ti-arrows-maximize fs-6"></i></a>
-                    <a href="javascript:void(0)" class="mb-0 link d-flex text-white align-items-center pe-0 cursor-pointer" data-action="close">
-                      <i class="ti ti-x fs-6"></i>
-                    </a>
+                <div class="card-header text-bg-primary d-flex align-items-center rounded-top-4">
+                    <h4 class="card-title text-white mb-0">Belajar Laravel 11</h4>
+                  <div class=" ms-auto d-flex">
+                    <span class="mb-1 badge bg-light text-dark">Mendatang</span>
                   </div>
                 </div>
                 <div class="card-body collapse show">
-                  <h4 class="card-title">Special title treatment</h4>
-                  <p class="card-text">
-                    With supporting text below as a natural lead-in to additional content.
-                  </p>
-                  <p class="card-text">
-                    With supporting text below as a natural lead-in to additional content.
-                  </p>
-                  <p class="card-text">
-                    With supporting text below as a natural lead-in to additional content.
-                  </p>
+                    <div class="d-flex justify-content-between mb-3">
+                        <div>
+                            <h6>12 Maret 2024</h6>
+                        </div>
+                        <div>
+                            <h6>08.00 - 09.00</h6>
+                        </div>
+                    </div>
+                    <div class="mx-3">
+                        <h6>Link Meet :</h6>
+                        <a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ipsam fugiat tenetur.</a>
+                    </div>
                 </div>
-              </div>
+            </div>
         </div>
+        <div class="col-4">
+            <div class="card">
+                <div class="card-header text-bg-warning d-flex align-items-center rounded-top-4">
+                  <h4 class="card-title text-white mb-0">Belajar Laravel 11</h4>
+                  <div class=" ms-auto d-flex">
+                    <span class="mb-1 badge bg-light text-dark">Berakhir</span>
+                  </div>
+                </div>
+                <div class="card-body collapse show">
+                    <div class="d-flex justify-content-between mb-3">
+                        <div>
+                            <h6>12 Maret 2024</h6>
+                        </div>
+                        <div>
+                            <h6>08.00 - 09.00</h6>
+                        </div>
+                    </div>
+                    <div class="mx-3">
+                        <h6>Link Meet :</h6>
+                        <a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ipsam fugiat tenetur.</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 @endsection
