9c5ecc3572bad1e891bd0a08922fdfd9795656af
nesaathussholeha <nesaathussholeha@gmail.com>
2024-04-02 20:28:49 +0700

    Update zoomschedule.blade.php

--- resources/views/mentor/zoomschedule.blade.php
+++ resources/views/mentor/zoomschedule.blade.php
@@ -12,55 +12,55 @@
     </div>
 </div>
 
-    <div class="row">
-        <div class="col-4">
-            <div class="card">
-                <div class="card-header text-bg-primary d-flex align-items-center rounded-top-4">
-                    <h4 class="card-title text-white mb-0">Belajar Laravel 11</h4>
-                  <div class=" ms-auto d-flex">
+<div class="row">
+    <div class="col-lg-4 col-md-6 col-sm-12 mb-4"> <!-- Menambahkan kelas grid untuk responsivitas -->
+        <div class="card h-100"> <!-- Menambahkan kelas h-100 untuk mengisi tinggi kartu -->
+            <div class="card-header text-bg-primary d-flex align-items-center rounded-top-4">
+                <h4 class="card-title text-white mb-0">Belajar Laravel 11</h4>
+                <div class="ms-auto d-flex">
                     <span class="mb-1 badge bg-light text-dark">Mendatang</span>
-                  </div>
                 </div>
-                <div class="card-body collapse show">
-                    <div class="d-flex justify-content-between mb-3">
-                        <div>
-                            <h6>12 Maret 2024</h6>
-                        </div>
-                        <div>
-                            <h6>08.00 - 09.00</h6>
-                        </div>
+            </div>
+            <div class="card-body collapse show">
+                <div class="d-flex justify-content-between mb-3">
+                    <div>
+                        <h6>12 Maret 2024</h6>
                     </div>
-                    <div class="mx-3">
-                        <h6>Link Meet :</h6>
-                        <a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ipsam fugiat tenetur.</a>
+                    <div>
+                        <h6>08.00 - 09.00</h6>
                     </div>
+                </div>
+                <div class="mx-3">
+                    <h6>Link Meet :</h6>
+                    <a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ipsam fugiat tenetur.</a>
                 </div>
             </div>
         </div>
-        <div class="col-4">
-            <div class="card">
-                <div class="card-header text-bg-warning d-flex align-items-center rounded-top-4">
-                  <h4 class="card-title text-white mb-0">Belajar Laravel 11</h4>
-                  <div class=" ms-auto d-flex">
-                    <span class="mb-1 badge bg-light text-dark">Berakhir</span>
-                  </div>
-                </div>
-                <div class="card-body collapse show">
-                    <div class="d-flex justify-content-between mb-3">
-                        <div>
-                            <h6>12 Maret 2024</h6>
-                        </div>
-                        <div>
-                            <h6>08.00 - 09.00</h6>
-                        </div>
-                    </div>
-                    <div class="mx-3">
-                        <h6>Link Meet :</h6>
-                        <a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ipsam fugiat tenetur.</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
     </div>
+    <div class="col-lg-4 col-md-6 col-sm-12 mb-4"> <!-- Menambahkan kelas grid untuk responsivitas -->
+        <div class="card h-100"> <!-- Menambahkan kelas h-100 untuk mengisi tinggi kartu -->
+            <div class="card-header text-bg-warning d-flex align-items-center rounded-top-4">
+                <h4 class="card-title text-white mb-0">Belajar Laravel 11</h4>
+                <div class="ms-auto d-flex">
+                    <span class="mb-1 badge bg-light text-dark">Berakhir</span>
+                </div>
+            </div>
+            <div class="card-body collapse show">
+                <div class="d-flex justify-content-between mb-3">
+                    <div>
+                        <h6>12 Maret 2024</h6>
+                    </div>
+                    <div>
+                        <h6>08.00 - 09.00</h6>
+                    </div>
+                </div>
+                <div class="mx-3">
+                    <h6>Link Meet :</h6>
+                    <a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ipsam fugiat tenetur.</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
