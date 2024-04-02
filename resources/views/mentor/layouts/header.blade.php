698675d174f4a45f43d94aa7e829f51675363e32
nesaathussholeha <nesaathussholeha@gmail.com>
2024-04-02 20:18:06 +0700

    update header mentor

--- resources/views/mentor/layouts/header.blade.php
+++ resources/views/mentor/layouts/header.blade.php
@@ -1,17 +1,31 @@
 <header class="app-header">
     <nav class="navbar navbar-expand-lg navbar-light">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link sidebartoggler nav-icon-hover ms-n3" id="headerCollapse" href="javascript:void(0)">
-            <i class="ti ti-menu-2"></i>
-          </a>
-        </li>
-      </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link sidebartoggler nav-icon-hover ms-n3" id="headerCollapse" href="javascript:void(0)">
+                    <i class="ti ti-menu-2"></i>
+                </a>
+            </li>
+            <li class="nav-item d-none d-lg-block">
+                <a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#exampleModal" class="nav-link nav-icon-hover">
+                    <i class="ti ti-search"></i>
+                </a>
+            </li>
+        </ul>
+        <div class="d-block d-lg-none">
+            <img src="{{ asset('logopkldark.png') }}" class="dark-logo" width="160" alt="" />
+            <img src="{{ asset('assets/images/logo-pkl.png') }}" class="light-logo" width="160" style="display: none;" alt="" />
+        </div>
+        <button class="navbar-toggler p-0 border-0 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="p-2">
+                <i class="ti ti-dots fs-7"></i>
+            </span>
+        </button>
 
       <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <div class="d-flex align-items-center justify-content-between">
           <a href="javascript:void(0)" class="nav-link d-flex d-lg-none align-items-center justify-content-center" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobilenavbar" aria-controls="offcanvasWithBothOptions">
-            <i class="ti ti-align-justified fs-7"></i>
+            {{-- <i class="ti ti-align-justified fs-7"></i> --}}
           </a>
           <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-center">
             <li class="nav-item dropdown">
