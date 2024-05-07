10526d0c123f9657760c19eba054a8cab583a94d
nesaathussholeha <nesaathussholeha@gmail.com>
2024-05-07 15:33:54 +0700

    api mentor student

---
+++ app/Http/Controllers/Api/DashboardController copy.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Mentor;
+
+use App\Contracts\Interfaces\DivisionInterface;
+use App\Contracts\Interfaces\MentorDivisionInterface;
+use App\Contracts\Interfaces\MentorInterface;
+use App\Contracts\Interfaces\MentorStudentInterface;
+use App\Contracts\Interfaces\StudentInterface;
+use App\Contracts\Interfaces\ZoomScheduleInterface;
+use App\Http\Controllers\Controller;
+use App\Models\Division;
+use App\Models\MentorStudent;
+use App\Services\MentorService;
+use App\Services\StudentService;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    private DivisionInterface $division;
+    private MentorInterface $mentor;
+    private StudentInterface $student;
+    private StudentService $studentService;
+    private MentorStudentInterface $mentorStudent;
+    private MentorDivisionInterface $mentorDivision;
+    private ZoomScheduleInterface $zoomSchedule;
+    private MentorService $mentorService;
+
+    public function __construct(DivisionInterface $division, MentorInterface $mentor, StudentInterface $student, StudentService $studentService, MentorStudentInterface $mentorStudent, MentorDivisionInterface $mentorDivision, ZoomScheduleInterface $zoomSchedule, MentorService $mentorService)
+    {
+        $this->division = $division;
+        $this->mentor = $mentor;
+        $this->student = $student;
+        $this->studentService = $studentService;
+        $this->mentorStudent = $mentorStudent;
+        $this->mentorService = $mentorService;
+        $this->mentorDivision = $mentorDivision;
+        $this->zoomSchedule = $zoomSchedule;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $division = $this->division->get();
+        $mentor = $this->mentor->get();
+
+        $mentorStudent = $this->mentorStudent->whereMentorStudent(auth()->user()->mentor->id);
+        $jumlahSiswa = $mentorStudent->count();
+
+        $mentorDivision = $this->mentorDivision->get();
+        $check_id = $this->mentorStudent->pluck('student_id');
+
+        $zoomSchedule = $this->zoomSchedule->get();
+        $totalSchedule = $zoomSchedule->count();
+
+        return view('mentor.index',compact('division','mentor', 'mentorStudent','mentorDivision','jumlahSiswa','totalSchedule','zoomSchedule'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Contracts\Interfaces\MentorStudentInterface;
 use App\Contracts\Interfaces\ZoomScheduleInterface;
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\MentorStudentResource;
+use App\Models\MentorStudent;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     private ZoomScheduleInterface $zoom;
+    private MentorStudentInterface $mentorStudent;
 
-    public function __construct(ZoomScheduleInterface $zoom)
+    public function __construct(ZoomScheduleInterface $zoom, MentorStudentInterface $mentorStudent)
     {
         $this->zoom = $zoom;
+        $this->mentorStudent = $mentorStudent;
     }
 
     public function index()
@@ -20,5 +26,13 @@ class DashboardController extends Controller
         $zoom = $this->zoom->get();
         $data['result'] = $zoom;
         return response()->json($data, 200);
+    }
+
+    public function mentorStudent()
+    {
+        $mentorStudent = $this->mentorStudent->whereMentorStudent(auth()->user()->mentor->id);
+
+            return ResponseHelper::success(MentorStudentResource::collection($mentorStudent));
+
     }
 }
