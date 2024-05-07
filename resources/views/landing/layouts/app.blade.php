d93b1eca5ec8fbd8945f600c7fca617bedd2d73b
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-05-07 22:20:24 +0700

    Update app.blade.php

--- resources/views/landing/layouts/app.blade.php
+++ resources/views/landing/layouts/app.blade.php
@@ -11,19 +11,8 @@
     <meta name="keywords" content="Hummatech - Ayo bergabung dengan hummatech">
     <meta name="robots" content="INDEX,FOLLOW">
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="57x57" href="assets_landing/img/favicons/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="assets_landing/img/favicons/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="assets_landing/img/favicons/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="assets_landing/img/favicons/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="assets_landing/img/favicons/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="assets_landing/img/favicons/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="assets_landing/img/favicons/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="assets_landing/img/favicons/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="assets_landing/img/favicons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('mobilelogo.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('mobilelogo.png') }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('mobilelogo.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('mobilelogo.png') }}">
+    <link rel="shortcut icon" type="image/png"
+        href="{{ asset('mobilelogo.png') }}" />
     <link rel="manifest" href="assets_landing/img/favicons/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="assets_landing/img/favicons/ms-icon-144x144.png">
