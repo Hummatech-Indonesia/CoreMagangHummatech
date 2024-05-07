d4537357dbe80a212d8460094c3c172886de1ae0
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-05-07 22:29:15 +0700

    Update index.blade.php

--- resources/views/student_online_&_offline/course-store/index.blade.php
+++ resources/views/student_online_&_offline/course-store/index.blade.php
@@ -1,50 +1,59 @@
 @extends(auth()->user()->hasRole('siswa-online') ? 'student_online.layouts.app' : 'student_offline.layouts.app')
 
 @section('content')
-<div class="card bg-light-info shadow-none position-relative overflow-hidden">
-    <div class="card-body px-4 py-3">
-        <div class="row align-items-center">
-            <div class="col-9">
-                <h4 class="fw-semibold mb-8">Beli Materi</h4>
-                <nav aria-label="breadcrumb mt-2">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a class="text-muted " href="/siswa-offline">Dasborard</a></li>
-                        <li class="breadcrumb-item" aria-current="page">Beli Materi</li>
-                    </ol>
-                </nav>
-            </div>
-            <div class="col-3">
-                <div class="text-center mb-n5">
-                    <img src="https://pkl-hummatech.dev.id/assets-user/dist/images/breadcrumb/ChatBc.png" alt="Image" class="img-fluid mb-n4" />
+    <div class="card bg-light-info shadow-none position-relative overflow-hidden">
+        <div class="card-body px-4 py-3">
+            <div class="row align-items-center">
+                <div class="col-9">
+                    <h4 class="fw-semibold mb-8">Beli Materi</h4>
+                    <nav aria-label="breadcrumb mt-2">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a class="text-muted " href="/siswa-offline">Dasborard</a></li>
+                            <li class="breadcrumb-item" aria-current="page">Beli Materi</li>
+                        </ol>
+                    </nav>
+                </div>
+                <div class="col-3">
+                    <div class="text-center mb-n5">
+                        <img src="https://pkl-hummatech.dev.id/assets-user/dist/images/breadcrumb/ChatBc.png" alt="Image"
+                            class="img-fluid mb-n4" />
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="row">
-    @foreach ($courses as $course)
-        <div class="col-md-4 col-xxl-3">
-            <div class="card card-body p-3">
-                <img src="{{ asset('storage/' . $course->image) }}" class="rounded-1 mb-3 w-100" />
+    <div class="row">
+        @forelse ($courses as $course)
+            <div class="col-md-4 col-xxl-3">
+                <div class="card card-body p-3">
+                    <img src="{{ asset('storage/' . $course->image) }}" class="rounded-1 mb-3 w-100" />
 
 
-                <a href="{{ url('/courses/detail') }}">
-                    <h1 class="h2 fw-bolder">{{ $course->title }}</h1>
-                </a>
+                    <a href="{{ url('/courses/detail') }}">
+                        <h1 class="h2 fw-bolder">{{ $course->title }}</h1>
+                    </a>
 
-                <p>{{ $course->description }}</p>
+                    <p>{{ $course->description }}</p>
 
-                <form action="" method="POST">
-                    @csrf
-                    <input type="hidden" name="id" value="{{ $course->id }}" />
-                </form>
-                <div class="d-flex flex-column flex-lg-row gap-2 w-100">
-                    <a href="{{ route('student.course.show', $course->id) }}" class="btn w-100 btn-lg btn-outline-primary">Detail</a>
-                    <a href="{{ route('transaction.checkout-course', $course->id) }}" class="btn btn-lg btn-primary w-100 btn-lg">Beli</a>
+                    <form action="" method="POST">
+                        @csrf
+                        <input type="hidden" name="id" value="{{ $course->id }}" />
+                    </form>
+                    <div class="d-flex flex-column flex-lg-row gap-2 w-100">
+                        <a href="{{ route('student.course.show', $course->id) }}"
+                            class="btn w-100 btn-lg btn-outline-primary">Detail</a>
+                        <a href="{{ route('transaction.checkout-course', $course->id) }}"
+                            class="btn btn-lg btn-primary w-100 btn-lg">Beli</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    @endforeach
-</div>
+        @empty
+            <div class="col-md-12 text-center">
+                <img src="{{ asset('assets-user/dist/images/products/empty-shopping-bag.gif') }}" alt="No Data"
+                    height="120px" />
+                <h3 class="text-center">Data Masih Kosong</h3>
+            </div>
+        @endforelse
+    </div>
 @endsection
