31f9dd746f5a931b50fe44400a20361c02e22ad0
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-05-08 15:32:39 +0700

    masuk jam kantor admin

--- app/Models/AttendanceRule.php
+++ app/Models/AttendanceRule.php
@@ -42,9 +42,7 @@ class AttendanceRule extends Model
     use HasFactory;
     protected $table = 'attendance_rules';
     protected $primaryKey = 'id';
-    public $incrementing = false;
-    public $keyType = 'char';
 
-    protected $guarded = [];
+    protected $guarded = ['id'];
     protected $fillable = ['day', 'checkin_starts', 'checkin_ends', 'break_starts', 'break_ends', 'return_starts', 'return_ends', 'checkout_starts', 'checkout_ends'];
 }
