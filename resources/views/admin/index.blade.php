b8089e83f70ff8516e5231c89c307c8146bb2fbd
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-05-08 21:43:34 +0700

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,7 +8,7 @@
         <div class="col-12 col-md-8">
             <div class="d-flex justify-content-end">
                 <button class="btn btn-success" data-bs-toggle="modal" data-bs-target=".myModal">Ubah Data Admin</button>
-                <button class="btn btn-success" data-bs-toggle="modal" data-bs-target=".myModal">Ubah Data CEO</button>
+                <button class="btn btn-success" data-bs-toggle="modal" data-bs-target=".myModal1">Ubah Data CEO</button>
             </div>
         </div>
     </div>
@@ -93,78 +93,92 @@
                         </div>
                     </form>
                 @endif
+
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
+    <div id="myModal" class="modal fade myModal1" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true"
+        style="display: none;">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="myModalLabel">Data Admin</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
                 @if ($dataceo == null)
-                    <form action="/dataceo/store" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <div class="modal-body">
-                            <div class="d-flex justify-content-center">
-                                <label class="form-label text-white" for="image-input1">
-                                    <img id="preview-image1" src="https://task.hummatech.com/assets/img/avatars/pen.png"
-                                        alt="example placeholder"
-                                        style="width: 150px; height: 150px; border-radius: 10px; cursor: pointer;object-fit: cover"
-                                        class="rounded-circle">
-                                    <input type="file" class="form-control d-none" id="image-input1" name="image"
-                                        accept="image/*">
-                                </label>
-                            </div>
-                            <div class="mb-2">
-                                <label for="">Nama</label>
-                                <input type="text" class="form-control" id="" name="name"
-                                    placeholder="Masukkan Nama">
-                            </div>
-                            <div class="mb-2">
-                                <label for="">PT</label>
-                                <input type="text" class="form-control" id="" name="company"
-                                    placeholder="Masukkan PT">
-                            </div>
-                            <div class="mb-2">
-                                <label for="">Jabatan</label>
-                                <input type="text" class="form-control" id="" name="field"
-                                    placeholder="Masukkan Jabatan">
-                            </div>
+                <form action="/dataceo/store" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="d-flex justify-content-center">
+                            <label class="form-label text-white" for="image-input1">
+                                <img id="preview-image1" src="https://task.hummatech.com/assets/img/avatars/pen.png"
+                                    alt="example placeholder"
+                                    style="width: 150px; height: 150px; border-radius: 10px; cursor: pointer;object-fit: cover"
+                                    class="rounded-circle">
+                                <input type="file" class="form-control d-none" id="image-input1" name="image"
+                                    accept="image/*">
+                            </label>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary ">Save Changes</button>
+                        <div class="mb-2">
+                            <label for="">Nama</label>
+                            <input type="text" class="form-control" id="" name="name"
+                                placeholder="Masukkan Nama">
                         </div>
-                    </form>
-                @else
-                    <form action="/dataceo/update/{{ $dataceo->id }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        @method('PUT')
-                        <div class="modal-body">
-                            <div class="d-flex justify-content-center">
-                                <label class="form-label text-white" for="image-input1">
-                                    <img id="preview-image1" src="{{ asset('storage/' . $dataadmin->image) }}"
-                                        alt="example placeholder"
-                                        style="width: 150px; height: 150px; border-radius: 10px; cursor: pointer;object-fit: cover"
-                                        class="rounded-circle">
-                                    <input type="file" class="form-control d-none" id="image-input1" name="image"
-                                        accept="image/*">
-                                </label>
-                            </div>
-                            <div class="mb-2">
-                                <label for="">Nama</label>
-                                <input type="text" class="form-control" value="{{ $dataadmin->name }}" id="" name="name"
-                                    placeholder="Masukkan Nama">
-                            </div>
-                            <div class="mb-2">
-                                <label for="">PT</label>
-                                <input type="text" class="form-control" id="" value="{{ $dataadmin->company }}" name="company"
-                                    placeholder="Masukkan PT">
-                            </div>
-                            <div class="mb-2">
-                                <label for="">Jabatan</label>
-                                <input type="text" class="form-control" id="" value="{{ $dataadmin->field }}" name="field"
-                                    placeholder="Masukkan Jabatan">
-                            </div>
+                        <div class="mb-2">
+                            <label for="">PT</label>
+                            <input type="text" class="form-control" id="" name="company"
+                                placeholder="Masukkan PT">
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary ">Save Changes</button>
+                        <div class="mb-2">
+                            <label for="">Jabatan</label>
+                            <input type="text" class="form-control" id="" name="field"
+                                placeholder="Masukkan Jabatan">
                         </div>
-                    </form>
-                @endif
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary ">Save Changes</button>
+                    </div>
+                </form>
+            @else
+                <form action="/dataceo/update/{{ $dataceo->id }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <div class="modal-body">
+                        <div class="d-flex justify-content-center">
+                            <label class="form-label text-white" for="image-input1">
+                                <img id="preview-image1" src="{{ asset('storage/' . $dataadmin->image) }}"
+                                    alt="example placeholder"
+                                    style="width: 150px; height: 150px; border-radius: 10px; cursor: pointer;object-fit: cover"
+                                    class="rounded-circle">
+                                <input type="file" class="form-control d-none" id="image-input1" name="image"
+                                    accept="image/*">
+                            </label>
+                        </div>
+                        <div class="mb-2">
+                            <label for="">Nama</label>
+                            <input type="text" class="form-control" value="{{ $dataadmin->name }}" id="" name="name"
+                                placeholder="Masukkan Nama">
+                        </div>
+                        <div class="mb-2">
+                            <label for="">PT</label>
+                            <input type="text" class="form-control" id="" value="{{ $dataadmin->company }}" name="company"
+                                placeholder="Masukkan PT">
+                        </div>
+                        <div class="mb-2">
+                            <label for="">Jabatan</label>
+                            <input type="text" class="form-control" id="" value="{{ $dataadmin->field }}" name="field"
+                                placeholder="Masukkan Jabatan">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary ">Save Changes</button>
+                    </div>
+                </form>
+            @endif
+
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
