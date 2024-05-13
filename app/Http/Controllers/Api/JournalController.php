e2155a2008d8fd75865fb0d74573dfc9b2743861
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-05-13 20:40:19 +0700

    Update JournalController.php

--- app/Http/Controllers/Api/JournalController.php
+++ app/Http/Controllers/Api/JournalController.php
@@ -9,6 +9,7 @@ use App\Models\Journal;
 use App\Services\JournalService;
 use App\Http\Requests\StoreJournalRequest;
 use App\Http\Requests\UpdateJournalRequest;
+use Illuminate\Http\JsonResponse;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use App\Contracts\Interfaces\JournalInterface;
 use App\Contracts\Interfaces\DataAdminInterface;
@@ -60,7 +61,12 @@ class JournalController extends Controller
         }
     }
 
-    public function update(UpdateJournalRequest $request, Journal $journal)
+    /**
+     * @param UpdateJournalRequest $request
+     * @param Journal $journal
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(UpdateJournalRequest $request, Journal $journal): JsonResponse
     {
         $this->journal->update($journal->id, $request->validated());
         return ResponseHelper::success(null, 'berhasil');
