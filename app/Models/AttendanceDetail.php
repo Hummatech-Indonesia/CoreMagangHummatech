9c0e55d3e424a5ee8776a4fdf6dffe35b52f0529
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-05-13 17:11:32 +0700

    update created at

--- app/Models/AttendanceDetail.php
+++ app/Models/AttendanceDetail.php
@@ -7,31 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-/**
- * 
- *
- * @property int $id
- * @property int $attendance_id
- * @property string $status
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \App\Models\Attendance $attendance
- * @method static \Illuminate\Database\Eloquent\Builder|AttendanceDetail newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|AttendanceDetail newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|AttendanceDetail query()
- * @method static \Illuminate\Database\Eloquent\Builder|AttendanceDetail whereAttendanceId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AttendanceDetail whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AttendanceDetail whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AttendanceDetail whereStatus($value)
- * @method static \Illuminate\Database\Eloquent\Builder|AttendanceDetail whereUpdatedAt($value)
- * @mixin \Eloquent
- */
 class AttendanceDetail extends Model implements HasAttendance
 {
     use HasFactory;
 
     protected $table = 'attendance_details';
-    protected $fillable = ['attendance_id', 'status'];
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+    protected $fillable = ['attendance_id', 'status', 'created_at', 'updated_at'];
     protected $guarded = [];
 
     /**
