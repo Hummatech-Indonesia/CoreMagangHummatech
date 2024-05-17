15bbda7aa0e2bd8bc6f43828d50f427a4737a2e1
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-05-17 23:20:29 +0700

    Update index.blade.php

--- resources/views/mentor/Journal/index.blade.php
+++ resources/views/mentor/Journal/index.blade.php
@@ -149,7 +149,6 @@
                             @endforelse
                         </tbody>
                     </table>
-                    {{ $journalStudents->links() }}
                 </div>
             </div>
 
