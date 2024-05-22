967e5790217d2bb6a3f0cdc36e40eb02be706e7e
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-05-22 16:08:57 +0700

    Update index.blade.php

--- resources/views/student_offline/journal/index.blade.php
+++ resources/views/student_offline/journal/index.blade.php
@@ -281,7 +281,7 @@
                     <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <div class="col-lg-6 text-start" id="detail-content">
+                    <div class="col-lg-12 text-start" id="detail-content">
                     </div>
                 </div>
                 <div class="modal-footer">
