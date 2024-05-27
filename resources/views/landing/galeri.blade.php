cb72b55afa0a02cacc9dcd1af91ba824fcb75686
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-05-27 13:31:30 +0700

    Create galeri.blade.php

---
+++ resources/views/landing/galeri.blade.php
@@ -0,0 +1,143 @@
+@extends('landing.layouts.app')
+@section('content')
+<div class="breadcumb-wrapper" data-bg-src="assets_landing/img/galeri/gallery.png">
+    <div class="container">
+        <div class="breadcumb-content">
+            <h1 class="breadcumb-title">Galeri</h1>
+            <ul class="breadcumb-menu">
+                <li><a href="/">Home</a></li>
+                <li>Galeri</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<section class="space">
+    <div class="container th-container4">
+        <div class="title-area text-center">
+            <span class="sub-title">
+                <div class="icon-masking me-2">
+                    <span class="mask-icon" data-mask-src="assets_landing/img/galeri/theme/title_shape_1.svg"></span>
+                    <img src="assets_landing/img/galeri/theme/title_shape_1.svg" alt="shape">
+                </div>GALERI
+            </span>
+            <h2 class="sec-title">Galeri <span class="text-theme">Hummatech</span></h2>
+            <p>Semua Kegiatan yang sudah terjadi dan telah diabadikan di Hummatech</p>
+        </div>
+        <div id="yearly" class="wrapper-full">
+            <div class="row gy-40 justify-content-center">
+                <div class="col-xl-4 col-md-6">
+                    <div class="blog-card h-100">
+                        <div class="blog-img">
+                            <img src="assets_landing/img/galeri/galeri1.jpg" alt="blog image" class="img-fluid"
+                                style="height: 200px; object-fit: cover;">
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="blog.html">
+                                    <i class="fal fa-calendar-days"></i>15
+                                    Jan, 2024</a>
+                            </div>
+                            <h3 class="box-title">Foto Bersama setelah Magang</a></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6">
+                    <div class="blog-card h-100">
+                        <div class="blog-img">
+                            <img src="assets_landing/img/galeri/galeri1.jpg" alt="blog image" class="img-fluid"
+                                style="height: 200px; object-fit: cover;">
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="blog.html">
+                                    <i class="fal fa-calendar-days"></i>15
+                                    Jan, 2024</a>
+                            </div>
+                            <h3 class="box-title">Liburan untuk siswa magang Hummatech</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6">
+                    <div class="blog-card h-100">
+                        <div class="blog-img">
+                            <img src="assets_landing/img/galeri/galeri1.jpg" alt="blog image" class="img-fluid"
+                                style="height: 200px; object-fit: cover;">
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="blog.html">
+                                    <i class="fal fa-calendar-days"></i>15
+                                    Jan, 2024</a>
+                            </div>
+                            <h3 class="box-title">Liburan untuk siswa magang Hummatech</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6">
+                    <div class="blog-card h-100">
+                        <div class="blog-img">
+                            <img src="assets_landing/img/galeri/galeri1.jpg" alt="blog image" class="img-fluid"
+                                style="height: 200px; object-fit: cover;">
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="blog.html">
+                                    <i class="fal fa-calendar-days"></i>15
+                                    Jan, 2024</a>
+                            </div>
+                            <h3 class="box-title">Liburan untuk siswa magang Hummatech</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6">
+                    <div class="blog-card h-100">
+                        <div class="blog-img">
+                            <img src="assets_landing/img/galeri/galeri1.jpg" alt="blog image" class="img-fluid"
+                                style="height: 200px; object-fit: cover;">
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="blog.html">
+                                    <i class="fal fa-calendar-days"></i>15
+                                    Jan, 2024</a>
+                            </div>
+                            <h3 class="box-title">Liburan untuk siswa magang Hummatech</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6">
+                    <div class="blog-card h-100">
+                        <div class="blog-img">
+                            <img src="assets_landing/img/galeri/galeri1.jpg" alt="blog image" class="img-fluid"
+                                style="height: 200px; object-fit: cover;">
+                        </div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <a href="blog.html">
+                                    <i class="fal fa-calendar-days"></i>15
+                                    Jan, 2024</a>
+                            </div>
+                            <h3 class="box-title">Liburan untuk siswa magang Hummatech</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="th-pagination mt-5 text-center">
+                <ul>
+                    <li><a href="blog.html">1</a></li>
+                    <li><a href="blog.html">2</a></li>
+                    <li><a href="blog.html">3</a></li>
+                    <li><a href="blog.html"><i class="far fa-arrow-right"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+@endsection
