e9bd8c3773feadfd962afc04c687ab522bb034bc
nesaathussholeha <nesaathussholeha@gmail.com>
2024-05-28 09:50:25 +0700

    Update PermissionController.php

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -72,69 +72,33 @@ class PermissionController extends Controller
      * @param  mixed $request
      * @return RedirectResponse
      */
-    // public function updateApproval(Permission $permission, PermissionRequest $request): RedirectResponse
-    // {
-    //     $this->permission->update($permission->id, $request->all());
-    //     if ($request->status_approval == StatusApprovalPermissionEnum::AGREE->value) {
-    //         if ($permission->start === Carbon::today()->toDateString()) {
-    //             $izinDari = Carbon::tomorrow();
-    //         } else {
-    //             $izinDari = Carbon::parse($permission->start);
-    //         }
-    //         $izinSampai = Carbon::parse($permission->end);
-    //         $tanggalMulai = $izinDari;
-    //         $tanggalBerakhir = $izinSampai;
-    //         $today = now();
-    //         while ($tanggalMulai <= $tanggalBerakhir) {
-    //             $this->attendance->store([
-    //                 'student_id' => $permission->student_id,
-    //                 'status' => $permission->status,
-    //                 'created_at' => $today,
-    //             ]);
-    //             $tanggalMulai->addDay();
-    //             $today->addDay();
-    //         }
-    //     }
-    //     return redirect()->back()->with('success', 'Berhasil menyimpan');
-    // }
-
     public function updateApproval(Permission $permission, PermissionRequest $request): RedirectResponse
     {
         $this->permission->update($permission->id, $request->all());
-
-        $latestPermission = Permission::find($permission->id);
-
-        // Cek jika status_approval adalah "AGREE"
-        if ($latestPermission->status_approval == StatusApprovalPermissionEnum::AGREE->value) {
-            $startDate = Carbon::parse($latestPermission->start);
-            $endDate = Carbon::parse($latestPermission->end);
-
-            $dates = $this->generateDateRange($startDate, $endDate);
-
-            foreach ($dates as $date) {
-                Attendance::create([
-                    'student_id' => $latestPermission->student_id,
-                    'status' => $latestPermission->status,
-                    'created_at' => $date,
-                    'updated_at' => $date,
+        if ($request->status_approval == StatusApprovalPermissionEnum::AGREE->value) {
+            if ($permission->start === Carbon::today()->toDateString()) {
+                $izinDari = Carbon::tomorrow();
+            } else {
+                $izinDari = Carbon::parse($permission->start);
+            }
+            $izinSampai = Carbon::parse($permission->end);
+            $tanggalMulai = $izinDari;
+            $tanggalBerakhir = $izinSampai;
+            $today = now();
+            while ($tanggalMulai <= $tanggalBerakhir) {
+                $this->attendance->store([
+                    'student_id' => $permission->student_id,
+                    'status' => $permission->status,
+                    'created_at' => $today,
                 ]);
+                $tanggalMulai->addDay();
+                $today->addDay();
             }
         }
-
         return redirect()->back()->with('success', 'Berhasil menyimpan');
     }
 
-    private function generateDateRange(Carbon $startDate, Carbon $endDate): array
-    {
-        $dates = [];
 
-        while ($startDate <= $endDate) {
-            $dates[] = $startDate->copy();
-            $startDate->addDay();
-        }
-
-        return $dates;
-    }
 
     public function updateApprovalReject(Permission $permission, PermissionRequest $request): RedirectResponse
     {
