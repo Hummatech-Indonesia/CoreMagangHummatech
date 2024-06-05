06bede3d4940a9d3400388b11408412ed81301eb
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-06-05 13:10:08 +0700

    Update active-course.blade.php

--- resources/views/student_online_&_offline/course/active-course.blade.php
+++ resources/views/student_online_&_offline/course/active-course.blade.php
@@ -15,7 +15,8 @@
             </div>
             <div class="col-3">
                 <div class="text-center mb-n5">
-                    <img src="https://pkl-hummatech.dev.id/assets-user/dist/images/breadcrumb/ChatBc.png" alt="Image" class="img-fluid mb-n4" />
+                    <img src="https://pkl-hummatech.dev.id/assets-user/dist/images/breadcrumb/ChatBc.png" alt="Image"
+                        class="img-fluid mb-n4" />
                 </div>
             </div>
         </div>
@@ -40,8 +41,31 @@
                     <input type="hidden" name="id" value="{{ $course->id }}" />
                 </form>
                 <div class="d-flex flex-column flex-lg-row gap-2 w-100">
-                    <a href="{{ route('student.course.show', $course->course->id) }}" class="btn w-100 btn-lg btn-outline-primary">Detail</a>
-                    <a href="{{ route('transaction.checkout-course', $course->course->id) }}" class="btn btn-lg btn-primary w-100 btn-lg">Beli</a>
+                    <a href="{{ route('student.course.show', $course->course->id) }}"
+                        class="btn w-100 btn-lg btn-outline-primary">Detail</a>
+                    <!-- <a href="{{ route('transaction.checkout-course', $course->course->id) }}" class="btn btn-lg btn-primary w-100 btn-lg">Beli</a> -->
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-xxl-3">
+            <div class="card card-body p-3">
+                <img src="{{ asset('assets-user/images/laravel-11.jpg') }}" class="rounded-1 mb-3 w-100" />
+
+
+                <a href="{{ url('/courses/detail') }}">
+                    <h1 class="h2 fw-bolder">{{ $course->course->title }}</h1>
+                </a>
+
+                <p>{{ $course->course->description }}</p>
+
+                <form action="" method="POST">
+                    @csrf
+                    <input type="hidden" name="id" value="{{ $course->id }}" />
+                </form>
+                <div class="d-flex flex-column flex-lg-row gap-2 w-100">
+                    <a href="{{ route('student.course.show', $course->course->id) }}"
+                        class="btn w-100 btn-lg btn-outline-primary">Detail</a>
+                    <!-- <a href="{{ route('transaction.checkout-course', $course->course->id) }}" class="btn btn-lg btn-primary w-100 btn-lg">Beli</a> -->
                 </div>
             </div>
         </div>
