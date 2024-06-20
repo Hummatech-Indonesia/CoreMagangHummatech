00d80b4b0936b3cb473e92306322227d78158da8
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-06-20 11:44:17 +0700

    Update NoteController.php

--- app/Http/Controllers/Api/NoteController.php
+++ app/Http/Controllers/Api/NoteController.php
@@ -48,7 +48,7 @@ class NoteController extends Controller
         $data = $request->validated();
         $data['hummatask_team_id'] = $hummataskTeam->id;
         $categoryBoard = $this->categoryBoard->store($data);
-        dd($data['name']);
+        dd(json_decode($data['name']));
         foreach (json_decode($data['name']) as $title) {
             $dataBoard['name'] = $title;
             $dataBoard['category_board_id'] = $categoryBoard->id;
