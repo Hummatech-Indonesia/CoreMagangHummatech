1bef40ce0deb9e410c2443ce2feed56f8a59f57d
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-06-20 09:01:51 +0700

    Update PresentationController.php

--- app/Http/Controllers/Api/PresentationController.php
+++ app/Http/Controllers/Api/PresentationController.php
@@ -109,6 +109,7 @@ class PresentationController extends Controller
         }
 
         if ($myPresentation = $this->presentation->getByTeamToday($teamLeader == null ? $teamMember->id : $teamLeader->id)) {
+            dd($myPresentation->status);
             if ($myPresentation->status == StatusPresentationEnum::PENNDING->value || $myPresentation->status == null) {
                 $this->presentation->update($myPresentation->id, ['status_presentation' => StatusPresentationEnum::ONGOING->value]);
             }
