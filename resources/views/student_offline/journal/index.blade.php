fddedf927c057de95526b0e13f3dcaeda3780477
nesaathussholeha <nesaathussholeha@gmail.com>
2024-08-25 15:20:10 +0700

    update modal jurnal siswa

--- resources/views/student_offline/journal/index.blade.php
+++ resources/views/student_offline/journal/index.blade.php
@@ -1,4 +1,21 @@
 @extends(auth()->user()->hasRole('siswa-online') ? 'student_online.layouts.app' : 'student_offline.layouts.app')
+
+<style>
+    @media (max-width: 767.98px) {
+        .modal-dialog {
+            margin: 0 auto;
+            max-width: 100%;
+        }
+    }
+
+    /* Optional: Ensure modal body is scrollable */
+    .modal-dialog-scrollable .modal-body {
+        max-height: calc(100vh - 210px);
+        /* Adjust height as needed */
+        overflow-y: auto;
+    }
+</style>
+
 @section('content')
     <div class="card bg-light-info shadow-none position-relative overflow-hidden">
         <div class="card-body px-4 py-3">
@@ -27,9 +44,7 @@
         <div class="modal-dialog modal-dialog-scrollable modal-lg">
             <div class="modal-content">
                 <div class="modal-header d-flex align-items-center">
-                    <h4 class="modal-title" id="myLargeModalLabel">
-                        Tambah Jurnal
-                    </h4>
+                    <h4 class="modal-title" id="myLargeModalLabel">Tambah Jurnal</h4>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form action="/create/jurnal" method="post" enctype="multipart/form-data">
@@ -39,9 +54,7 @@
                         <label for="" class="mt-2 mb-2">Judul</label>
                         <input type="text" name="title" class="form-control" value="{{ old('title') }}">
                         @error('title')
-                            <p class="text-danger">
-                                {{ $message }}
-                            </p>
+                            <p class="text-danger">{{ $message }}</p>
                         @enderror
                         <label for="" class="mt-2 mb-2">Bukti</label>
                         <figure class="col-xl-3 col-md-4 col-6" itemprop="associatedMedia" itemscope="">
@@ -49,72 +62,47 @@
                         </figure>
                         <input type="file" name="image" class="form-control" onchange="preview(event)">
                         @error('image')
-                            <p class="text-danger">
-                                {{ $message }}
-                            </p>
+                            <p class="text-danger">{{ $message }}</p>
                         @enderror
                         <label for="" class="mt-2 mb-2">Deskripsi</label>
-                        <textarea name="description" id="description" class="form-control" onkeyup="countCharacters(this)"></textarea>
+                        <textarea name="description" id="description" class="form-control" rows="3" onkeyup="countCharacters(this)"></textarea>
                         <p id="characterCount">0 characters</p>
                         @error('description')
-                            <p class="text-danger">
-                                {{ $message }}
-                            </p>
+                            <p class="text-danger">{{ $message }}</p>
                         @enderror
                     </div>
-                    <hr>
-                    <div class="modal-footer">
+                    <div class="modal-footer border border-top-gray">
                         <button type="button" class="btn btn-light-danger text-danger font-medium waves-effect text-start"
-                            data-bs-dismiss="modal">
-                            Close
-                        </button>
+                            data-bs-dismiss="modal">Tutup</button>
                         <button type="submit"
-                            class="btn btn-light-primary text-primary font-medium waves-effect text-start"
-                            data-bs-dismiss="modal">
-                            Simpan
-                        </button>
+                            class="btn btn-light-primary text-primary font-medium waves-effect text-start">Simpan</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
 
-    <style>
-        .modal-dialog {
-            max-width: 90%;
-        }
 
-        @media (min-width: 576px) {
-            .modal-dialog {
-                max-width: 500px;
-            }
-        }
 
-        @media (min-width: 992px) {
-            .modal-dialog {
-                max-width: 800px;
-            }
-        }
-    </style>
     <div class="d-flex justify-content-between mb-4">
         <h4>
             Data Jurnal
         </h4>
         <div class="d-flex">
-            {{-- <a href="/download-pdf-JurnalSiswa" class="kamu-tak-diajak w-full">
+            <a href="/download-pdf-JurnalSiswa" class="kamu-tak-diajak w-full">
                 <button type="button" class="btn mb-1 btn-light-warning text-warning btn-lg px-4 fs-4 font-medium ms-3">
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
-                            class="h-5 w-5 ltr:mr-2 rtl:ml-2">
-                            <path
-                                d="M15.3929 4.05365L14.8912 4.61112L15.3929 4.05365ZM19.3517 7.61654L18.85 8.17402L19.3517 7.61654ZM21.654 10.1541L20.9689 10.4592V10.4592L21.654 10.1541ZM3.17157 20.8284L3.7019 20.2981H3.7019L3.17157 20.8284ZM20.8284 20.8284L20.2981 20.2981L20.2981 20.2981L20.8284 20.8284ZM14 21.25H10V22.75H14V21.25ZM2.75 14V10H1.25V14H2.75ZM21.25 13.5629V14H22.75V13.5629H21.25ZM14.8912 4.61112L18.85 8.17402L19.8534 7.05907L15.8947 3.49618L14.8912 4.61112ZM22.75 13.5629C22.75 11.8745 22.7651 10.8055 22.3391 9.84897L20.9689 10.4592C21.2349 11.0565 21.25 11.742 21.25 13.5629H22.75ZM18.85 8.17402C20.2034 9.3921 20.7029 9.86199 20.9689 10.4592L22.3391 9.84897C21.9131 8.89241 21.1084 8.18853 19.8534 7.05907L18.85 8.17402ZM10.0298 2.75C11.6116 2.75 12.2085 2.76158 12.7405 2.96573L13.2779 1.5653C12.4261 1.23842 11.498 1.25 10.0298 1.25V2.75ZM15.8947 3.49618C14.8087 2.51878 14.1297 1.89214 13.2779 1.5653L12.7405 2.96573C13.2727 3.16993 13.7215 3.55836 14.8912 4.61112L15.8947 3.49618ZM10 21.25C8.09318 21.25 6.73851 21.2484 5.71085 21.1102C4.70476 20.975 4.12511 20.7213 3.7019 20.2981L2.64124 21.3588C3.38961 22.1071 4.33855 22.4392 5.51098 22.5969C6.66182 22.7516 8.13558 22.75 10 22.75V21.25ZM1.25 14C1.25 15.8644 1.24841 17.3382 1.40313 18.489C1.56076 19.6614 1.89288 20.6104 2.64124 21.3588L3.7019 20.2981C3.27869 19.8749 3.02502 19.2952 2.88976 18.2892C2.75159 17.2615 2.75 15.9068 2.75 14H1.25ZM14 22.75C15.8644 22.75 17.3382 22.7516 18.489 22.5969C19.6614 22.4392 20.6104 22.1071 21.3588 21.3588L20.2981 20.2981C19.8749 20.7213 19.2952 20.975 18.2892 21.1102C17.2615 21.2484 15.9068 21.25 14 21.25V22.75ZM21.25 14C21.25 15.9068 21.2484 17.2615 21.1102 18.2892C20.975 19.2952 20.7213 19.8749 20.2981 20.2981L21.3588 21.3588C22.1071 20.6104 22.4392 19.6614 22.5969 18.489C22.7516 17.3382 22.75 15.8644 22.75 14H21.25ZM2.75 10C2.75 8.09318 2.75159 6.73851 2.88976 5.71085C3.02502 4.70476 3.27869 4.12511 3.7019 3.7019L2.64124 2.64124C1.89288 3.38961 1.56076 4.33855 1.40313 5.51098C1.24841 6.66182 1.25 8.13558 1.25 10H2.75ZM10.0298 1.25C8.15538 1.25 6.67442 1.24842 5.51887 1.40307C4.34232 1.56054 3.39019 1.8923 2.64124 2.64124L3.7019 3.7019C4.12453 3.27928 4.70596 3.02525 5.71785 2.88982C6.75075 2.75158 8.11311 2.75 10.0298 2.75V1.25Z"
-                                fill="currentColor" />
-                            <path opacity="0.5"
-                                d="M13 2.5V5C13 7.35702 13 8.53553 13.7322 9.26777C14.4645 10 15.643 10 18 10H22"
-                                stroke="currentColor" stroke-width="1.5" />
-                        </svg>
+                        class="h-5 w-5 ltr:mr-2 rtl:ml-2">
+                        <path
+                            d="M15.3929 4.05365L14.8912 4.61112L15.3929 4.05365ZM19.3517 7.61654L18.85 8.17402L19.3517 7.61654ZM21.654 10.1541L20.9689 10.4592V10.4592L21.654 10.1541ZM3.17157 20.8284L3.7019 20.2981H3.7019L3.17157 20.8284ZM20.8284 20.8284L20.2981 20.2981L20.2981 20.2981L20.8284 20.8284ZM14 21.25H10V22.75H14V21.25ZM2.75 14V10H1.25V14H2.75ZM21.25 13.5629V14H22.75V13.5629H21.25ZM14.8912 4.61112L18.85 8.17402L19.8534 7.05907L15.8947 3.49618L14.8912 4.61112ZM22.75 13.5629C22.75 11.8745 22.7651 10.8055 22.3391 9.84897L20.9689 10.4592C21.2349 11.0565 21.25 11.742 21.25 13.5629H22.75ZM18.85 8.17402C20.2034 9.3921 20.7029 9.86199 20.9689 10.4592L22.3391 9.84897C21.9131 8.89241 21.1084 8.18853 19.8534 7.05907L18.85 8.17402ZM10.0298 2.75C11.6116 2.75 12.2085 2.76158 12.7405 2.96573L13.2779 1.5653C12.4261 1.23842 11.498 1.25 10.0298 1.25V2.75ZM15.8947 3.49618C14.8087 2.51878 14.1297 1.89214 13.2779 1.5653L12.7405 2.96573C13.2727 3.16993 13.7215 3.55836 14.8912 4.61112L15.8947 3.49618ZM10 21.25C8.09318 21.25 6.73851 21.2484 5.71085 21.1102C4.70476 20.975 4.12511 20.7213 3.7019 20.2981L2.64124 21.3588C3.38961 22.1071 4.33855 22.4392 5.51098 22.5969C6.66182 22.7516 8.13558 22.75 10 22.75V21.25ZM1.25 14C1.25 15.8644 1.24841 17.3382 1.40313 18.489C1.56076 19.6614 1.89288 20.6104 2.64124 21.3588L3.7019 20.2981C3.27869 19.8749 3.02502 19.2952 2.88976 18.2892C2.75159 17.2615 2.75 15.9068 2.75 14H1.25ZM14 22.75C15.8644 22.75 17.3382 22.7516 18.489 22.5969C19.6614 22.4392 20.6104 22.1071 21.3588 21.3588L20.2981 20.2981C19.8749 20.7213 19.2952 20.975 18.2892 21.1102C17.2615 21.2484 15.9068 21.25 14 21.25V22.75ZM21.25 14C21.25 15.9068 21.2484 17.2615 21.1102 18.2892C20.975 19.2952 20.7213 19.8749 20.2981 20.2981L21.3588 21.3588C22.1071 20.6104 22.4392 19.6614 22.5969 18.489C22.7516 17.3382 22.75 15.8644 22.75 14H21.25ZM2.75 10C2.75 8.09318 2.75159 6.73851 2.88976 5.71085C3.02502 4.70476 3.27869 4.12511 3.7019 3.7019L2.64124 2.64124C1.89288 3.38961 1.56076 4.33855 1.40313 5.51098C1.24841 6.66182 1.25 8.13558 1.25 10H2.75ZM10.0298 1.25C8.15538 1.25 6.67442 1.24842 5.51887 1.40307C4.34232 1.56054 3.39019 1.8923 2.64124 2.64124L3.7019 3.7019C4.12453 3.27928 4.70596 3.02525 5.71785 2.88982C6.75075 2.75158 8.11311 2.75 10.0298 2.75V1.25Z"
+                            fill="currentColor" />
+                        <path opacity="0.5"
+                            d="M13 2.5V5C13 7.35702 13 8.53553 13.7322 9.26777C14.4645 10 15.643 10 18 10H22"
+                            stroke="currentColor" stroke-width="1.5" />
+                    </svg>
                     PDF
                 </button>
-            </a> --}}
+            </a>
 
             <button type="button" class="btn mb-1 btn-light-primary text-primary btn-lg px-4 fs-4 font-medium ms-3"
                 data-bs-toggle="modal" data-bs-target="#staticBackdrop">
@@ -263,7 +251,8 @@
                             </p>
                         @enderror
                         <label for="description-edit" class="mt-2 mb-2">Deskripsi</label>
-                        <textarea name="description" id="description-edit" class="form-control" oninput="countCharactersEdit(this)"></textarea>
+                        <textarea name="description" id="description-edit" class="form-control" rows="3"
+                            oninput="countCharactersEdit(this)"></textarea>
                         <div id="characterCountEdit"></div>
                         @error('description')
                             <p class="text-danger">
@@ -289,17 +278,16 @@
         </div>
     </div>
 
+    <!-- Modal -->
     <div class="modal fade modal-bookmark" id="detail" tabindex="-1" role="dialog"
         aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
             <div class="modal-content px-2">
                 <div class="modal-header border-bottom">
                     <h5 class="modal-title me-2" id="exampleModalLabel">Detail Jurnal</h5>
                     <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <div class="col-lg-12 text-start" id="detail-content">
-                    </div>
+                <div id="detail-content">
                 </div>
                 <div class="modal-footer">
                     <div class="d-flex justify-content-end">
@@ -323,7 +311,7 @@
             var description = $(this).data('description').replace(/<br\s*\/?>/gi, "\n");
             var image = $(this).data('image');
 
-            $('#form-update').attr('action', '/journal/' + id);
+            $('#form-update').attr('action', '/siswa-offline/journal/' + id);
             $('#title-edit').val(title);
             $('#description-edit').val(description);
             $('#image-edit').attr('src', image);
@@ -378,7 +366,7 @@
             modalBody.append('<div class="mb-2">');
             modalBody.append('<h6 class="f-w-600">Bukti</h6>');
             modalBody.append('<img src="' + image +
-                '" class="img-fluid" style="width:100%; max-width:700px; object-fit:cover"></img>');
+                '" class="img-fluid" style="width:50%; max-width:300px; object-fit:cover">');
             modalBody.append('</div>');
             detail.append(modalBody);
             $('#detail').modal('show');
