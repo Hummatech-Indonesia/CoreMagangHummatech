209db805dd21570bcf381a92b5fedfd45afe6cee
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-09-17 09:50:20 +0700

    Update PermissionController.php

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -40,7 +40,7 @@ class PermissionController extends Controller
     public function index(Request $request)
     {
         $students = $this->student->get($request);
-        $permissions = $this->permission->search($request)->paginate(10);
+        $permissions = $this->permission->search($request)->paginate(10)->latest();
         $perPagePanding = 10;
         $perPageAgree = 10;
         $perPageReject = 10;
