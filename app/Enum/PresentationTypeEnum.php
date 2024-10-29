f75d495c11e4d485182c78dd766c9a5f845df658
AlulCode45 <alulgans70@gmail.com>
2024-10-29 16:53:55 +0700

    [add] Adding enums for handle presentation

--- app/Enum/PresentationTypeEnum.php
+++ app/Enum/PresentationTypeEnum.php
@@ -1,9 +1,11 @@
 <?php
-
-enum ProjectTypeEnum: string
+namespace App\Enum;
+enum PresentationTypeEnum: string
 {
     case SOLO = 'solo project';
     case PREMINI = 'premini project';
     case MINI = 'mini project';
     case BIG = 'big project';
+    case INTERVIEW = 'interview';
+    case LIVECODING = 'livecoding';
 }
--- app/Enum/StatusMemberTeamEnum.php
+++ app/Enum/StatusMemberTeamEnum.php
@@ -1,1 +1,6 @@
 <?php
+namespace App\Enum;
+enum StatusMemberTeamEnum: string{
+    case Member = 'Member';
+    case Leader = 'Leader';
+}
