2c6065479447508bc2ef55526c690687b8909ab0
Kling777 <2good4u991@gmail.com>
2024-11-01 23:43:53 +0700

    change zoom-schedule search back to input

--- resources/views/admin/page/zoom-schedules/index.blade.php
+++ resources/views/admin/page/zoom-schedules/index.blade.php
@@ -1,37 +1,18 @@
 @extends('admin.layouts.app')
 @section('content')
-    <div class="card">
-        <div class="card-body">
-            <div class="row g-2">
-                <div class="col-sm-4">
-                    <h4 class="mx-5 pt-2">Jadwal Zoom</h4>
-                </div>
-                <div class="col-sm-auto d-flex ms-auto">
-                    <form style="width: 300px; margin-top: 5px; margin-right: 10px" action="/administrator/zoom-schedules">
-                        <div class="search-box d-flex mx-3">
-                            <select class="js-example-basic-single" name="title">
-                                <option value="" disabled {{ request()->title ? '' : 'selected' }}>Cari Zoom...
-                                </option>
-                                @forelse ($zoomSchedulesSearch as $zoomSchedule)
-                                    <option value="{{ $zoomSchedule->title }}"
-                                        {{ request()->title == $zoomSchedule->title ? 'selected' : '' }}>
-                                        {{ $zoomSchedule->title }}
-                                    </option>
-                                @empty
-                                @endforelse
-                            </select>
-                            <button class="btn btn-primary btn-sm ms-1" type="submit">Cari</button>
-                            <button class="btn btn-info btn-sm ms-1" type="button"
-                                onclick="window.location.href='/administrator/zoom-schedules';"><svg
-                                    class="bi bi-arrow-repeat" xmlns="http://www.w3.org/2000/svg" width="16"
-                                    height="16" fill="currentColor" viewBox="0 0 16 16">
-                                    <path
-                                        d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9" />
-                                    <path fill-rule="evenodd"
-                                        d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z" />
-                                </svg></button>
-                        </div>
-                    </form>
+<div class="card">
+    <div class="card-body">
+        <div class="row g-2">
+            <div class="col-sm-4">
+                <h4 class="mx-5 pt-2">Jadwal Zoom</h4>
+            </div>
+            <div class="col-sm-auto ms-auto d-flex">
+                <form action="/administrator/zoom-schedules">
+                    <div class="search-box mx-3">
+                        <input type="text" class="form-control" name="title" value="{{request()->title}}" id="searchMemberList" placeholder="Cari Jadwal...">
+                        <i class="ri-search-line search-icon"></i>
+                    </div>
+                </form>
                     <div class="list-grid-nav hstack gap-1">
                         <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#add">
                             Tambah Data
