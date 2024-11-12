edc38efc5492dd4a6905ea51d62c8abe00353638
AlulCode45 <alulgans70@gmail.com>
2024-11-12 15:31:28 +0700

    fix: Upcoming Project

--- app/Contracts/Repositories/ProjectRepository.php
+++ app/Contracts/Repositories/ProjectRepository.php
@@ -45,18 +45,22 @@ class ProjectRepository extends BaseRepository implements ProjectInterface
     {
         return $this->model->query()->create($data);
     }
+
     public function update(mixed $id, array $data): mixed
     {
         return $this->model->query()->findOrFail($id)->update($data);
     }
+
     public function delete(mixed $id): mixed
     {
         return $this->model->query()->findOrFail($id)->delete($id);
     }
+
     public function where($parameter, $value): mixed
     {
         return $this->model->query()->where($parameter, $value)->get();
     }
+
     public function accProject(mixed $id, array $data, $hummataskTeam): mixed
     {
         $data['start_date'] = Carbon::now()->toDateString();
@@ -68,6 +72,7 @@ class ProjectRepository extends BaseRepository implements ProjectInterface
             ->delete();
         return $this->model->query()->findOrFail($id)->update($data);
     }
+
     public function getProjectAccepted($id): mixed
     {
         return $this->model->query()->where('hummatask_team_id', $id)->where('status', '!=', StatusProjectEnum::PENDING->value)->first();
@@ -76,43 +81,37 @@ class ProjectRepository extends BaseRepository implements ProjectInterface
     public function getQueueProjectPresentation($id): mixed
     {
         $data = $this->model->query()
-            ->whereHas('presentation', function($query) use ($id){
+            ->whereHas('presentation', function ($query) use ($id) {
                 $query->where('project_id', $id);
             })
-            ->orderBy('created_at','DESC')
+            ->orderBy('created_at', 'DESC')
             ->first();
         return $data->urutan ?? 0;
     }
+
     public function upcomingproject(int $userId): mixed
     {
         $lastProject = $this->model->query()
             ->whereHas('members', function ($query) use ($userId) {
                 $query->where('member_id', $userId);
             })
-            ->orderBy('created_at', 'desc')
-            ->first()->type_project ?? '';
-        switch ($lastProject) {
-            case PresentationTypeEnum::SOLO->value:
-                return 5;
-            case PresentationTypeEnum::PREMINI->value:
-                return 4;
-            case PresentationTypeEnum::INTERVIEW->value:
-                return 3;
-            case PresentationTypeEnum::LIVECODING->value:
-                return 2;
-            case PresentationTypeEnum::MINI->value:
-                return 1;
-            case PresentationTypeEnum::BIG->value:
-                return 0;
-            default:
-                return 6;
-        }
+            ->orderBy('id', 'desc')
+            ->first()->type_project->value ?? '';
+        return match ($lastProject) {
+            PresentationTypeEnum::SOLO->value => 5,
+            PresentationTypeEnum::PREMINI->value => 4,
+            PresentationTypeEnum::INTERVIEW->value => 3,
+            PresentationTypeEnum::LIVECODING->value => 2,
+            PresentationTypeEnum::MINI->value => 1,
+            PresentationTypeEnum::BIG->value => 0,
+            default => 6,
+        };
     }
 
     public function show($id): mixed
     {
         return $this->model->query()
-            ->with(['presentation','members','members.members'])
+            ->with(['presentation', 'members', 'members.members'])
             ->findOrFail($id);
     }
 }
