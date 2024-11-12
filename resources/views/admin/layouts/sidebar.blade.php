463ff645571925a9fc00e48350a485a303fdf61e
nesaathussholeha <nesaathussholeha@gmail.com>
2024-11-12 11:21:30 +0700

    Update sidebar.blade.php

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -245,11 +245,11 @@
                                     class="nav-link {{ request()->is('students-banned*') ? 'active' : '' }}"
                                     data-key="t-range-slider">Banned Siswa</a>
                             </li>
-                            <li class="nav-item">
+                            {{-- <li class="nav-item">
                                 <a href="{{ url('/email-user') }}"
                                     class="nav-link {{ request()->is('email-user*') ? 'active' : '' }}"
                                     data-key="t-range-slider">Email user</a>
-                            </li>
+                            </li> --}}
                         </ul>
                     </div>
                 </li>
