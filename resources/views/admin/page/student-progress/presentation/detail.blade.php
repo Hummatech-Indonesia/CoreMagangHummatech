3443ea1d9e85cce37e8a924793f24d99457b3abc
MarshandaGra <marshandagracella.15@gmail.com>
2024-11-22 13:24:16 +0700

    updating admin student progress

--- resources/views/admin/page/student-progress/presentation/detail.blade.php
+++ resources/views/admin/page/student-progress/presentation/detail.blade.php
@@ -22,8 +22,8 @@
 @section('content')
     <div class="d-flex align-items-center justify-content-between mb-4">
         <!-- Tombol Kembali -->
-        <button class="btn btn-back py-3 px-3 me-3 d-flex align-items-center custom-card shadow-sm" style="background-color: #EAE9FF; border-radius: 8px;">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="#695EEF">
+        <button class="btn btn-back py-3 px-3 me-3 d-flex align-items-center custom-card shadow-sm" style="background-color: rgba(234, 233, 255, 1); border-radius: 8px;">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="rgba(105, 94, 239, 1)">
                 <path fill="none" d="M0 0h24v24H0z"></path>
                 <path d="M7.82843 10.9999H20V12.9999H7.82843L13.1924 18.3638L11.7782 19.778L4 11.9999L11.7782 4.22168L13.1924 5.63589L7.82843 10.9999Z"></path>
             </svg>
@@ -31,7 +31,7 @@
 
         <!-- Header Judul -->
         <div class="flex-grow-1 text-center py-3 px-3 rounded fw-bold custom-card shadow-sm" 
-            style="background-color: #EAE9FF; color: #695EEF; border-radius: 8px; ">
+            style="background-color: rgba(234, 233, 255, 1); color: rgba(105, 94, 239, 1); border-radius: 8px; ">
             Detail Presentasi
         </div>
     </div>
@@ -73,7 +73,12 @@
                         <div class="mb-3">
                             <label class="form-label text-9xl">Status</label>
                             <div>
-                                <span class="badge bg-success text-light px-3 py-2">Selesai Presentasi</span>
+                                <span class="badge text-success px-3 py-2" style="background-color: rgba(230, 255, 250, 1); border-radius:34px;">Selesai Presentasi</span>
+                            {{-- @if ()
+                                    <span class="badge text-success px-3 py-2" style="background-color: rgba(230, 255, 250, 1);  border-radius:34px;">Selesai Presentasi</span>
+                                @else
+                                    <span class="badge text-danger px-3 py-2" style="background-color: rgba(251, 242, 239, 1); border-radius:34px;">Belum Presentasi</span>
+                                @endif --}}
                             </div>
                         </div>
     
--- resources/views/admin/page/student-progress/project/index.blade.php
+++ resources/views/admin/page/student-progress/project/index.blade.php
@@ -1,24 +1,82 @@
 @extends('admin.layouts.app')
+@section('style')
+    <style>
+        .type-project {
+            background-color: rgba(212, 208, 255, 1);
+            color: rgba(105, 94, 239, 1);
+            border-radius: 3px;
+            text-align: center;
+            font-weight: bolder;
+            padding-inline: 6px;
+            padding-block: 12px;
+        }
+        .antrian-wrapper {
+            display: flex; 
+            align-items: center; 
+            gap: 8px;
+        }
+
+        .antrian {
+            background-color: rgb(124, 120, 120);
+            color: white;
+            padding: 10px;
+            margin:0px;
+            border-radius: 3px;
+            text-align: center; 
+            min-width: 40px; 
+            font-size: 16px;
+            font-weight: bold;
+            height: 40px;
+        }
+
+        .btn-delete {
+            background-color: rgba(247, 49, 100, 1);
+            border: none; 
+            border-radius: 3px;
+            padding: 10px;
+            height: 40px;;
+            color: white;
+            display: flex; 
+            align-items: center; 
+            justify-content: center;
+            cursor: pointer; 
+            box-shadow: none;
+        }
+
+        .btn-delete svg {
+            display: block;
+        }
+        
+        .status {
+            background-color: rgba(251, 242, 239, 1);
+            padding: 8px;
+            border-radius: 3px;
+            color: rgba(247, 49, 100, 1);
+            height: 27px;
+            min-width: 40px;
+            width: 72px;
+        }
+    </style>
+@endsection
 @section('content')
 <div class="row">
     {{-- @forelse ($categoryProjects as $categoryProject) --}}
-        <div class="col-lg-5">
+        <div class="col-12 col-sm-6 col-md-4">
             <div class="card card-height-100">
                 <div class="card-body">
                     <div class="d-flex justify-content-between mb-3">
                         <div class="d-flex">
                             {{-- tipe project --}}
-                            <h6 class="bg-primary text-light p-2 rounded">
-                                Solo Project</h6>
+                            <h6 class="type-project">Solo Project</h6>
                         </div>
                         <div>
                             {{-- Nomor antrian dan tombol delete --}}
-                            <div class="d-flex flex-shrink-0 align-items-center justify-content-center">
+                            <div class="antrian-wrapper">
                                 <!-- Nomor Antrian -->
-                                <h6 class="bg-dark text-light p-2 m-1 rounded">01</h6>
-                        
+                                <h6 class="antrian">01</h6>
+                            
                                 <!-- Tombol Delete -->
-                                <button class="bg-primary text-white border-0 p-2 m-1 rounded d-flex align-items-center justify-content-center" data-id="">
+                                <button class="btn btn-delete" data-id="">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
                                         <path fill="none" d="M0 0h24v24H0z"></path>
                                         <path d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z"></path>
@@ -29,9 +87,9 @@
                     </div>
                     {{-- judul project dan status  --}}
                     <div class="d-flex justify-content-between">
-                        <h4> Web Rental Mobil</h4>
+                        <h4 class="fw-bolder mb-0"> Web Rental Mobil</h4>
                         <div class="gap-2">
-                            <span class="badge bg-danger p-2"> Telat 1 Hari</span>
+                            <span class="badge status"> Telat 1 Hari</span>
                             {{-- @if ()
                                 <span class="badge bg-success"></span>
                             @else
@@ -47,7 +105,7 @@
 
                     {{-- untuk avatar --}}
                     <div class="avatar-sm flex-shrink-0">
-                        <span class="avatar-title bg-success-subtle rounded-circle fs-2 mt-2">
+                        <span class="avatar-title bg-success-subtle rounded-circle fs-2 mt-2" style="height:35px; width:38px; padding:2px">
                             <img src="{{ asset('berkas/logo.png') }}" alt="" srcset=""
                                 class="avatar-title bg-success-subtle rounded-circle fs-2">
                         </span>
@@ -55,34 +113,31 @@
 
                     {{-- deskripsi proyek --}}
                     <div class="d-flex justify-content-between mt-4 mb-2">
-                        <h6 class="">Project yang mengerjakan web rental mobil</h6>
+                        <h5 class="fs-bolder">Project yang mengerjakan web rental mobil</h5>
                     </div>
 
                     {{-- body  --}}
                     <div class="d-flex justify-content-between">
                         <h6> Kondisi Proyek</h6>
                         <div class="gap-2">
-                            <span class="badge bg-success p-2"> Aktif</span>
-                            {{-- @if ()
-                                <span class="badge bg-success">Aktif</span>
+                            <span class="badge text-success p-8 pt-2" style="background-color: rgba(230, 255, 250, 1);  border-radius:7px; font-size:small; height:27px; width:68px;">Active</span>
+                            {{-- 
+                            @if ()
+                                <span class="badge text-success p-8 pt-2" style="background-color: rgba(230, 255, 250, 1);  border-radius:7px; font-size:small; height:27px; width:68px;">Active</span>
                             @else
-                                <span class="badge bg-danger">Pasif</span>
-                            @endif --}}
+                                <span class="badge text-danger p-8 pt-2" style="background-color:  rgba(251, 242, 239, 1);  border-radius:7px; font-size:small; height:27px; width:68px;">Deactive</span>
+                            @endif
+                            --}}
                         </div>
                     </div> 
                     
-                    <div class="d-flex justify-content-between pl-0 mt-1 mb-2">
-                        <h6> Deadline</h6>
+                    <div class="d-flex justify-content-between pl-0 mt-1 mb-3">
+                        <h6> Deadline :</h6>
                         <div class="gap-2">
-                            <span class="text-success"> Senin, 25 November 2024</span>
-                            {{-- @if ()
-                                <span class="text-success"></span>
-                            @else
-                                <span class="text-danger"></span>
-                            @endif --}}
+                            <span style="color: rgba(19, 222, 185, 1);"> Senin, 25 November 2024</span>
                         </div>
                     </div> 
-                    <button class="btn btn-primary btn-detail text-light w-100 rounded">Lihat Detail</button>
+                    <button class="btn btn-detail justify-content-center" style="background-color: rgba(105, 94, 239, 1); border-radius: 4px; color: white; width: 324px; height: 43;">Lihat Detail</button>
                 </div>
             </div>
         </div>
