166640e390731b1edaa8ae5f3487648a58724433
IqbalFirmans <iqbalclan74@gmail.com>
2024-11-25 01:15:45 +0700

    add pagination project submission, add  complete project tab

--- app/Contracts/Repositories/ProjectRepository.php
+++ app/Contracts/Repositories/ProjectRepository.php
@@ -58,15 +58,21 @@ class ProjectRepository extends BaseRepository implements ProjectInterface
         return $this->model->query()->findOrFail($id)->delete($id);
     }
 
-    public function where($parameter, $value): mixed
+    public function where($parameter, $value, $perPage = 15, $columns = ['*'], $pageName = 'page'): mixed
     {
-        return $this->model->query()->where($parameter, $value)->get();
+        return $this->model->query()
+            ->where($parameter, $value)
+            ->paginate($perPage, $columns, $pageName);
     }
 
-    public function whereIn($parameter, array $values): mixed
+    public function whereIn($parameter, array $values, $perPage = 15, $columns = ['*'], $pageName = 'page'): mixed
     {
-        return $this->model->query()->whereIn($parameter, $values)->get();
+        return $this->model->query()
+            ->whereIn($parameter, $values)
+            ->paginate($perPage, $columns, $pageName);
     }
+
+
 
     public function accProject(mixed $id, array $data): mixed
     {
---
+++ resources/views/mentor/project-submission2/partials/complete-submission.blade.php
@@ -0,0 +1,83 @@
+<div class="tab-pane {{ request()->has('complete_page') ? 'active' : '' }}" id="complete-project" role="tabpanel">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 g-4">
+        @forelse ($complete_projects as $project)
+            <div class="col">
+                <div class="card shadow-sm border-0">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center mb-3">
+                            <span class="badge bg-light-primary text-primary px-3 py-2 rounded-2 fw-bolder">
+                                {{ $project->type_project }}
+                            </span>
+                            <div class="d-flex gap-1">
+                                <a href="{{ route('mentor.project-submissions.show', $project->id) }}"
+                                    class="btn btn-primary text-white p-1">
+                                    <i class="ti ti-eye fs-7"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <h5 class="fw-semibold">{{ $project->project_name }}</h5>
+                        <p class="text-muted mb-1">By Kelompok {{ $project->members->first()?->members->name }}
+                        </p>
+                        <div class="mb-4">
+                            @php($members = $project->members)
+
+                            @if ($members->count() === 1)
+                                <div class="d-flex justify-content-start">
+                                    <img src="{{ asset('assets-user/dist/images/profile/user-2.jpg') }}" alt="Avatar"
+                                        class="rounded-circle shadow-sm img-fluid" width="33" height="33">
+                                </div>
+                            @elseif ($members->count() > 1)
+                                <div class="d-flex justify-content-start">
+                                    <ul class="hstack mb-0">
+                                        @foreach ($members as $index => $member)
+                                            <li class="{{ $index > 0 ? 'ms-n8' : '' }}">
+                                                <a href="javascript:void(0)" class="me-1" data-bs-toggle="tooltip"
+                                                    data-bs-placement="top" aria-label="{{ $member->members->name }}"
+                                                    data-bs-original-title="{{ $member->members->name }}">
+                                                    <img src="{{ asset('assets-user/dist/images/profile/user-1.jpg') }}"
+                                                        class="rounded-circle border border-2 border-white"
+                                                        width="33" height="33"
+                                                        alt="{{ $member->members->name }}">
+                                                </a>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+                        </div>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <p class="fw-bold text-black mb-0">Kondisi Project:</p>
+                            <span class="{{ $project->getProjectStatus()->color() }}  px-2 py-1 rounded-2">
+                                {{ $project->getProjectStatus()->label() }}
+                            </span>
+                        </div>
+
+
+                        <div class="d-flex justify-content-between align-items-center mt-1">
+                            <p class="text-black mb-0">Deadline:</p>
+                            <small class="text-success fw-semibold">
+                                {{ \Carbon\Carbon::parse($project->start_date)->format('d/m/Y') }} -
+                                {{ \Carbon\Carbon::parse($project->end_date)->format('d/m/Y') }}
+                            </small>
+                        </div>
+                        <div class="d-flex justify-content-between mt-4">
+                            <a class="btn btn-primary w-100"
+                                href="{{ route('mentor.project-submissions.show', $project->id) }}">Detail</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <div class="d-flex justify-content-center align-items-center" style="min-height: 300px; width: 100%;">
+                <div class="text-center">
+                    <img src="{{ asset('assets-user/dist/images/products/empty-shopping-bag.gif') }}" alt="No Data"
+                        height="120px" />
+                    <h3 class="mt-3">Data Masih Kosong</h3>
+                </div>
+            </div>
+        @endforelse
+
+
+    </div>
+    {{ $complete_projects->appends(['complete_page' => request('complete_page')])->links() }}
+</div>
