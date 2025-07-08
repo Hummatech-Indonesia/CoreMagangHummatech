315d51ef1c68c785bca82f246756c4831f3937ad
Farah Amalia <140934377+farahamaliaa@users.noreply.github.com>
2025-07-08 14:04:50 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,6 +60,16 @@
                             </div>
                         </div>
                         <div class="bs-stepper-content">
+                            @if ($errors->any())
+                                <div class="badge bg-light-danger text-danger p-3 w-100">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+                            <div class="badge bg-light-danger text-danger"></div>
                             <form id="multiStepsForm" method="POST" action="/register/post" enctype="multipart/form-data">
                                 @method('POST')
                                 @csrf
