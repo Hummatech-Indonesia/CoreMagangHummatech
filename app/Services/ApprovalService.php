a3ec6ff6db0e7a063321ea740aee990836d6b035
Farah Amalia <140934377+farahamaliaa@users.noreply.github.com>
2025-07-12 07:27:07 +0700

    Update ApprovalService.php

--- app/Services/ApprovalService.php
+++ app/Services/ApprovalService.php
@@ -149,9 +149,9 @@ class ApprovalService
         }
 
         if ($student->internship_type == InternshipTypeEnum::OFFLINE->value) {
-            $existingUser->assignRole(RolesEnum::OFFLINE->value);
+            $existingUser->syncRole(RolesEnum::OFFLINE->value);
         } elseif ($student->internship_type == InternshipTypeEnum::ONLINE->value) {
-            $existingUser->assignRole(RolesEnum::ONLINE->value);
+            $existingUser->syncRole(RolesEnum::ONLINE->value);
         }
 
         // Data For Update Status Students
